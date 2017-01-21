3a9f13a19c94cd021ee313295f5359aba8fa92dd
pdl5p <paul.lucas@fivep.com.au>
2017-01-22 10:15:06 +1100

    playing with collisions

--- src/components/clouds/cloud4.jsx
+++ src/components/clouds/cloud4.jsx
@@ -2,21 +2,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import * as d3 from 'd3';
+import collide from '../../common/collide.js';
 
 class Cloud4 extends Component {
 
     componentDidMount() {
         const self = this;
+        self.collide = collide;
         console.log("MOUNTED");
 
         const svg = d3.select(ReactDOM.findDOMNode(this));
 
         svg.on("mousemove", (e) => {
             let p = d3.mouse(svg.node())
-            //console.log("E", p);
-            root.px = p[0];
-            root.py = p[1];
-            force.resume();
+            //root.px = p[0];
+            //root.py = p[1];
+            //force.resume();
         });
 
 
@@ -25,7 +26,7 @@ class Cloud4 extends Component {
 
         var nodes = d3.range(200).map(function () { return { radius: Math.random() * 12 + 4 }; }),
             root = nodes[0],
-            color = d3.scale.category10();
+            color = d3.scale.category20c();
 
         root.radius = 0;
         root.fixed = true;
@@ -42,18 +43,29 @@ class Cloud4 extends Component {
             .data(nodes.slice(1))
             .enter().append("circle")
             .attr("r", function (d) { return d.radius; })
-            .style("fill", function (d, i) { return color(i % 3); });
+            .style("fill", function (d, i) { return color(i % 5); })
+            .on("click", function(d, i, n) {
 
-        force.on("tick", function (e) {
+                d.radius = d.radius * 3;
+                d3.select(this).attr("r", d.radius);
+                force.resume();
+
+            });
+
+        let recalc = (nodes, svg, collide) => {
             var q = d3.geom.quadtree(nodes),
                 i = 0,
                 n = nodes.length;
 
-            while (++i < n) q.visit(self.collide(nodes[i]));
+            while (++i < n) q.visit(collide(nodes[i]));
 
             svg.selectAll("circle")
                 .attr("cx", function (d) { return d.x; })
                 .attr("cy", function (d) { return d.y; });
+        }
+
+        force.on("tick", function (e) {
+            recalc(nodes, svg, self.collide);
         });
     }
 
@@ -66,31 +78,7 @@ class Cloud4 extends Component {
         );
     }
 
-    collide(node) {
-        var r = node.radius + 16,
-            nx1 = node.x - r,
-            nx2 = node.x + r,
-            ny1 = node.y - r,
-            ny2 = node.y + r;
 
-        return function (quad, x1, y1, x2, y2) {
-            if (quad.point && (quad.point !== node)) {
-                var x = node.x - quad.point.x,
-                    y = node.y - quad.point.y,
-                    l = Math.sqrt(x * x + y * y),
-                    r = node.radius + quad.point.radius;
-
-                if (l < r) {
-                    l = (l - r) / l * .5;
-                    node.x -= x *= l;
-                    node.y -= y *= l;
-                    quad.point.x += x;
-                    quad.point.y += y;
-                }
-            }
-            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-        }
-    }
 }
 
 export { Cloud4 };
